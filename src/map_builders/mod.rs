9402b2448aeaca64c7948e93658fe81a557362c9
Nikita Dmitriev <nikdmit8@gmail.com>
2024-09-24 17:00:32 +0400

    Binary Space Partition maps.

--- src/map_builders/mod.rs
+++ src/map_builders/mod.rs
@@ -1,6 +1,8 @@
 use super::{Map, Rect, TileType, Position, spawner, SHOW_MAPGEN_VISUALIZER};
 mod simple_map;
 use simple_map::SimpleMapBuilder;
+mod bsp_dungeon;
+use bsp_dungeon::BspDungeonBuilder;
 mod common;
 use common::*;
 use specs::prelude::*;
@@ -14,7 +16,11 @@ pub trait MapBuilder {
     fn take_snapshot(&mut self);
 }
 
-pub fn random_builder(new_depth : i32) -> Box<dyn MapBuilder> {
-    // Note that until we have a second map type, this isn't even slighlty random
-    Box::new(SimpleMapBuilder::new(new_depth))
+pub fn random_builder(new_depth: i32) -> Box<dyn MapBuilder> {
+    let mut rng = rltk::RandomNumberGenerator::new();
+    let builder = rng.roll_dice(1, 2);
+    match builder {
+        1 => Box::new(BspDungeonBuilder::new(new_depth)),
+        _ => Box::new(SimpleMapBuilder::new(new_depth))
+    }
 }
