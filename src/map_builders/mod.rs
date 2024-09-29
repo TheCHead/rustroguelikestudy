83af4f442cef63c8a57c3df574ae7c86c92adf5f
Nikita Dmitriev <nikdmit8@gmail.com>
2024-09-29 17:13:56 +0400

    Maze maps.

--- src/map_builders/mod.rs
+++ src/map_builders/mod.rs
@@ -11,6 +11,8 @@ mod drunkard;
 use drunkard::DrunkardsWalkBuilder;
 use drunkard::DrunkardSettings;
 use drunkard::DrunkSpawnMode;
+mod maze;
+use maze::MazeBuilder;
 mod common;
 use common::*;
 use specs::prelude::*;
@@ -26,7 +28,7 @@ pub trait MapBuilder {
 
 pub fn random_builder(new_depth: i32) -> Box<dyn MapBuilder> {
     // let mut rng = rltk::RandomNumberGenerator::new();
-    // let builder = rng.roll_dice(1, 7);
+    // let builder = rng.roll_dice(1, 8);
     // match builder {
     //     1 => Box::new(BspDungeonBuilder::new(new_depth)),
     //     2 => Box::new(BspInteriorBuilder::new(new_depth)),
@@ -34,8 +36,9 @@ pub fn random_builder(new_depth: i32) -> Box<dyn MapBuilder> {
     //     4 => Box::new(DrunkardsWalkBuilder::open_area(new_depth)),
     //     5 => Box::new(DrunkardsWalkBuilder::open_halls(new_depth)),
     //     6 => Box::new(DrunkardsWalkBuilder::winding_passages(new_depth)),
+    //     7 => Box::new(MazeBuilder::new(new_depth)),
     //     _ => Box::new(SimpleMapBuilder::new(new_depth))
     // }
 
-    Box::new(DrunkardsWalkBuilder::winding_passages(new_depth))
+    Box::new(MazeBuilder::new(new_depth))
 }
